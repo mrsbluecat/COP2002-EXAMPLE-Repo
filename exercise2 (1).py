567500f7eadaacb97d803db77c8330af2b6e5d7c
mrsbluecat <179531440+mrsbluecat@users.noreply.github.com>
2024-09-04 21:38:15 -0400

    Add files via upload

---
+++ exercise2 (1).py
@@ -0,0 +1,32 @@
+#Marie Carroll mrsbluecat
+#COP 2002 section M2
+#09/03/2024
+#Exercise 2: Variables
+#Produces the output provided in canvas exercise using my variables
+
+#variables for my information
+first_name = "marie"
+last_name = "carroll"
+major = "IT Security Risk Managment"
+interested = "it is my indroduction to the world of coding"
+x = 28
+y = 37
+
+#equations required
+a = x + y
+b = y - x
+c = x * y
+d = y / x
+e = y % x
+
+#code written to print the variables into the format specified
+print(f"My name is {first_name.title()} {last_name.title()}.  My major is {major}.\nI'm most interested in this class because {interested}.\n")
+print(f"{x} + {y} = {a}\n")
+print(f"{y} - {x} = {b}\n")
+print(f"{x} * {y} = {c}\n")
+print(f"{y} / {x} = {d}\n")
+print(f"{y} % {x} = {e}\n")
+print("This is the end of the program!")
+
+
+
